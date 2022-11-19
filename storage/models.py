f07d30fc08d7c8ee0da5ec5505b940323f4ae5b1
Amir Dashti <ahdashti@gmail.com>
2022-11-19 13:10:32 +0330

    veiw models to admin panel. and edit models

--- storage/models.py
+++ storage/models.py
@@ -3,23 +3,23 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
-    orginal_title = models.CharField(max_length=255)
-    year = models.DateField()
-    runtime = models.FloatField(max_length=3)
-    genre = models.CharField(max_length=255)
-    language = models.CharField(max_length=255)
-    country = models.CharField(max_length=255)
-    poster = models.ImageField()
-    imdb_id = models.CharField(max_length=20, unique=True)
+    orginal_title = models.CharField(max_length=255, null=True)
+    year = models.FloatField(max_length=4, null=True)
+    runtime = models.FloatField(max_length=3, null=True)
+    genre = models.CharField(max_length=255, null=True)
+    language = models.CharField(max_length=255, null=True)
+    country = models.CharField(max_length=255, null=True)
+    poster = models.ImageField(null=True)
+    imdb_id = models.CharField(max_length=20, unique=True, null=True)
 
 
 class Actor(models.Model):
-    full_name = models.ManyToManyField(Movie)
+    full_name = models.ManyToManyField(Movie, null=True)
 
 
 class Writer(models.Model):
-    full_name = models.ManyToManyField(Movie)
+    full_name = models.ManyToManyField(Movie, null=True)
 
 
 class Director(models.Model):
-    full_name = models.ManyToManyField(Movie)
+    full_name = models.ManyToManyField(Movie, null=True)
