e53924f721f74631c8ac4d77a886aaa5f630f342
Amir Dashti <ahdashti@gmail.com>
2022-11-20 01:24:01 +0330

    Serializing Objects

--- storage/views.py
+++ storage/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .models import Movie
+from .serializers import MovieSerializer
 
 
 @api_view()
 def movie_list(request):
-    return Response(('ok'))
+    quaryset = Movie.objects.all()
+    serializer = MovieSerializer(quaryset, many=True)
+    return Response(serializer.data)
 
 
 @api_view()
 def movie_detail(request, id):
-    return Response(id)
+        movie = get_object_or_404(Movie, pk=id)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
