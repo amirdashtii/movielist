b7f68b0304dcc2db99c22902d051e3621c611134
Amir Dashti <ahdashti@gmail.com>
2022-11-19 13:10:32 +0330

    veiw 'models' in admin panel. and edit models

--- storage/admin.py
+++ storage/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Movie, Actor, Director, Writer
 
-# Register your models here.
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['title', 'year']
+    list_editable = ['year']
+
+
+admin.site.register(Actor)
+admin.site.register(Director)
+admin.site.register(Writer)
--- storage/models.py
+++ storage/models.py
@@ -1,25 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Actor(models.Model):
+    full_name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.full_name
+
+
+class Writer(models.Model):
+    full_name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.full_name
+
+
+class Director(models.Model):
+    full_name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.full_name
 
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
-    orginal_title = models.CharField(max_length=255)
-    year = models.DateField()
-    runtime = models.FloatField(max_length=3)
-    genre = models.CharField(max_length=255)
-    language = models.CharField(max_length=255)
-    country = models.CharField(max_length=255)
-    poster = models.ImageField()
-    imdb_id = models.CharField(max_length=20, unique=True)
+    orginal_title = models.CharField(max_length=255, null=True)
+    year = models.DecimalField(max_digits=4,decimal_places=0, null=True)
+    runtime = models.DecimalField(max_digits=3, decimal_places=0, null=True)
+    genre = models.CharField(max_length=255, null=True)
+    language = models.CharField(max_length=255, null=True)
+    country = models.CharField(max_length=255, null=True)
+    poster = models.ImageField(null=True)
+    imdb_id = models.CharField(max_length=20, unique=True, null=True)
+    actor = models.ManyToManyField(Actor)
+    writer = models.ManyToManyField(Writer)
+    director = models.ManyToManyField(Director)
 
+    def __str__(self) -> str:
+        return self.title
 
-class Actor(models.Model):
-    full_name = models.ManyToManyField(Movie)
-
-
-class Writer(models.Model):
-    full_name = models.ManyToManyField(Movie)
-
-
-class Director(models.Model):
-    full_name = models.ManyToManyField(Movie)
+    class Meta:
+        ordering = ['title']
