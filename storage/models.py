0d781486a9868b28d3ad0296314bce60533ab591
Amir Dashti <ahdashti@gmail.com>
2022-11-19 08:04:19 +0330

    add 3 models : Actor Writre Director

--- storage/models.py
+++ storage/models.py
@@ -4,14 +4,22 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     orginal_title = models.CharField(max_length=255)
-    directors = models.TextField()
     year = models.DateField()
     runtime = models.FloatField(max_length=3)
-    writerr = models.TextField()
-    actors = models.TextField()
     genre = models.CharField(max_length=255)
     language = models.CharField(max_length=255)
     country = models.CharField(max_length=255)
     poster = models.ImageField()
     imdb_id = models.CharField(max_length=20, unique=True)
 
+
+class Actor(models.Model):
+    full_name = models.ManyToManyField(Movie)
+
+
+class Writer(models.Model):
+    full_name = models.ManyToManyField(Movie)
+
+
+class Director(models.Model):
+    full_name = models.ManyToManyField(Movie)
