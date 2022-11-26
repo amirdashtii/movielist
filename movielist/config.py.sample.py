6dcdd81ee3a162190baeb2d405ea0682ee77845e
Amir Dashti <ahdashti@gmail.com>
2022-11-26 12:38:15 +0330

    move to mysql

---
+++ movielist/config.py.sample.py
@@ -0,0 +1,8 @@
+# this is a sample config file. rename it to `config.py` and edit accordingly
+
+
+# Mysql config
+MYSQL_DB_NAME = 'your db name'
+MYSQL_HOST = 'your host'
+MYSQL_USER = 'your user'
+MYSQL_PASSWORD = 'your password'
