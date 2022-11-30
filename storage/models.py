b5bb6753b85cad8993f834f8042caeade09f6416
Amir Dashti <ahdashti@gmail.com>
2022-11-30 20:39:19 +0330

    edit movie model

--- storage/models.py
+++ storage/models.py
@@ -35,19 +35,26 @@ class Director(models.Model):
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     orginal_title = models.CharField(max_length=255, null=True, blank=True)
-    year = models.DecimalField(
-        max_digits=4, decimal_places=0, null=True, blank=True)
-    runtime = models.DecimalField(
-        max_digits=3, decimal_places=0, null=True, blank=True)
+    year = models.PositiveIntegerField(null=True, blank=True)
+    rated = models.CharField(max_length=255, null=True, blank=True)
+    released = models.CharField(max_length=255, null=True, blank=True)
+    runtime = models.PositiveIntegerField(null=True, blank=True)
     genre = models.CharField(max_length=255, null=True, blank=True)
+    director = models.ManyToManyField(Director, blank=True)
+    writer = models.ManyToManyField(Writer, blank=True)
+    actor = models.ManyToManyField(Actor, blank=True)
+    plot = models.TextField(null=True, blank=True)
     language = models.CharField(max_length=255, null=True, blank=True)
     country = models.CharField(max_length=255, null=True, blank=True)
-    poster = models.ImageField(null=True, blank=True)
+    awards = models.TextField(null=True, blank=True)
+    poster = models.URLField(max_length=200, null=True, blank=True)
+    ratings = models.TextField(null=True, blank=True)
+    metascore = models.PositiveIntegerField(null=True, blank=True)
+    imdbRating = models.DecimalField(
+        max_digits=2, decimal_places=1, null=True, blank=True)
     imdb_id = models.CharField(
         max_length=20, unique=True, null=True, blank=True)
-    actor = models.ManyToManyField(Actor, blank=True)
-    writer = models.ManyToManyField(Writer, blank=True)
-    director = models.ManyToManyField(Director, blank=True)
+    type = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self) -> str:
         return self.title
